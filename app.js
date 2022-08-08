8fc0289c26ef73b0a24978f45df2dc32391ea1d4
it_hongli <jyzhl11@163.com>
2022-08-08 18:51:46 +0800

    初始化

---
+++ app.js
@@ -0,0 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// 页面引擎配置
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//捕获404错误，并进行错误处理。
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// 错误处理
+app.use(function(err, req, res, next) {
+  // 设置locals，只在开发环境中返回错误信息
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // 渲染错误页面
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
